4b4cc2cbec66cadd23105e4ddae2eaab84850002
Jesus <jesusgranada99@gmail.com>
2021-09-21 14:23:29 +0300

    Add services interfaces

---
+++ wasted-app/Controller/Interfaces/Services/IBaseService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using wasted_app.Controller.Entities;
+
+namespace console_wasted_app.Controller.Interfaces
+{
+    public interface IBaseService<T> where T : BaseEntity
+    {
+        T getById(string Id);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(string id);
+    }
+}
---
+++ wasted-app/Controller/Interfaces/Services/IRestaurantService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using console_wasted_app.Controller.Entities;
+using console_wasted_app.Controller.Interfaces.Services;
+
+namespace console_wasted_app.Controller.Interfaces
+{
+    public interface IRestaurantService : IBaseService<Restaurant>, IAuthService
+    {
+        public IEnumerable<Restaurant> GetRestaurantsNear(Coords coords, int amount = 10);
+    }
+}
