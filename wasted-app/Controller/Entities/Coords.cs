e1d5875eefea4178081f86c9102d92a343b6a2fc
Jesus <jesusgranada99@gmail.com>
2021-09-24 16:33:12 +0300

    Add enum to identify distances

--- wasted-app/Controller/Entities/Coords.cs
+++ wasted-app/Controller/Entities/Coords.cs
@@ -3,13 +3,23 @@ namespace console_wasted_app.Controller.Entities
 {
     public struct Coords
     {
-        public decimal Latitude;
-        public decimal Altitude;
+        public decimal Latitude { get; set; }
+        public decimal Altitude { get; set; }
 
         public Coords( decimal latitude, decimal altitude )
         {
             Latitude = latitude;
             Altitude = altitude;
+        }
+
+        private decimal ManhattanDistance(Coords others)
+        {
+            return Math.Abs(Latitude - others.Latitude) + Math.Abs(Altitude - others.Altitude);
+        }
+
+        public bool IsNear(Coords others)
+        {
+            return (ManhattanDistance(others) <= (decimal)Distances.NEAR);
         }
     }
 }
