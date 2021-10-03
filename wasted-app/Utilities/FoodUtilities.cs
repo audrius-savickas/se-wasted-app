04245860d4b4fee601347a888d42b63e1888851d
audrius-savickas <umarasss@gmail.com>
2021-10-03 20:09:41 +0300

    add functionality for restaurants to add food

--- wasted-app/Utilities/FoodUtilities.cs
+++ wasted-app/Utilities/FoodUtilities.cs
@@ -6,9 +6,10 @@ namespace wasted_app.Utilities
 {
     internal class FoodUtilities
     {
+        private static ServicesController servicesController = ServicesController.Instance;
         public static IEnumerable<Food> GetFoodByRestaurantId(string restaurantId)
         {
-            var allFood = ServicesController.Instance.FoodService.GetAllFood();
+            var allFood = servicesController.FoodService.GetAllFood();
             var restaurantFood = new List<Food>();
 
             foreach (var food in allFood)
@@ -21,10 +22,49 @@ namespace wasted_app.Utilities
             return restaurantFood;
         }
 
-        public static string GetFoodTypeName(string typeId)
+        public static string GetFoodTypeNameById(string typeId)
         {
-            var foodType = ServicesController.Instance.TypeOfFoodService.GetByTypeOfFoodId(typeId);
+            var foodType = servicesController.TypeOfFoodService.GetByTypeOfFoodId(typeId);
             return foodType.Name;
+        }
+
+        public static string GetFoodTypeIdByName(string typeName)
+        {
+            var foodTypes = servicesController.TypeOfFoodService.GetAllTypesOfFood();
+            foreach(var foodType in foodTypes)
+            {
+                if (foodType.Name == typeName)
+                {
+                    return foodType.Id;
+                }
+            }
+            throw new System.Exception("Food type by name not found");
+        }
+
+        public static List<string> GetAllFoodTypesNames()
+        {
+            var foodTypes = servicesController.TypeOfFoodService.GetAllTypesOfFood();
+            var foodTypesNames = new List<string>();
+            foreach (var foodType in foodTypes)
+            {
+                foodTypesNames.Add(foodType.Name);
+            }
+            return foodTypesNames;
+        }
+
+        public static string GetFirstAvailableFoodId()
+        {
+            var foods = servicesController.FoodService.GetAllFood();
+            var index = 0;
+            foreach (var food in foods)
+            {
+                if (food.Id != index.ToString())
+                {
+                    return index.ToString();
+                }
+                index++;
+            }
+            return index.ToString();
         }
     }
 }
