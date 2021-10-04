b58166e69eb68fc9ad17e2bdca6a2d2d07175505
DominykasSip <90202955+DominykasSip@users.noreply.github.com>
2021-10-04 13:57:13 +0300

    fixed consistency in IAuthService

--- wasted-app/Controller/Interfaces/Services/IAuthService.cs
+++ wasted-app/Controller/Interfaces/Services/IAuthService.cs
@@ -5,7 +5,7 @@ namespace console_wasted_app.Controller.Interfaces.Services
     public interface IAuthService<T>
     {
         public bool Login(Credentials creds);
-        public bool Register(Credentials credentials, T obj);
+        public bool Register(Credentials creds, T obj);
         public void ChangePass(Mail email, Password newPassword);
         public void DeleteAccount(Credentials creds);
         public T GetByMail(Mail email);
