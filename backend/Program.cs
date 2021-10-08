761624a8eb49afd68e5e12b34cd60d325152f613
Jesus <jesusgranada99@gmail.com>
2021-10-08 21:19:30 +0300

    Change directory name and add missing files

---
+++ backend/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace console_wasted_app
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
--- wasted-app-backend/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace wasted_app_backend
-{
-    public class Class1
-    {
-    }
-}
