4da83464688f6a06c987ec500537a05a710a6eb5
Audrius Savickas <audriussav@wix.com>
2021-10-11 19:33:27 +0300

    revert commit

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,16 +6,6 @@ namespace backend
     class Program
     {
         static void Main(string[] args)
-        {
-            var servicesController = ServicesController.Instance;
-
-            var restaurantService = servicesController.RestaurantService;
-            var restaurants = restaurantService.GetAllRestaurants();
-
-            foreach ( var r in restaurants )
-            {
-                Console.WriteLine(r.Name);
-            }
-        }
+        { }
     }
 }
