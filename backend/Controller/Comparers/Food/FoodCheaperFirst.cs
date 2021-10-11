81a65e7c6643b6d694c3c350fd432ab679b1e1ed
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-10-11 12:24:57 +0300

    Style consistency

--- backend/Controller/Comparers/Food/FoodCheaperFirst.cs
+++ backend/Controller/Comparers/Food/FoodCheaperFirst.cs
@@ -5,7 +5,7 @@ namespace backend.Controller.Comparers.Food
     public class FoodCheaperFirst : IComparer<Entities.Food>
     {
         public FoodCheaperFirst()
-        {}
+        { }
 
         public int Compare(Entities.Food x, Entities.Food y)
         {
