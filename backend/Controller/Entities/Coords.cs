0b2d1d5f981b4c9cc87da5de087938ee6ba3459f
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-10-11 12:27:07 +0300

    Add new algorythm to get the real distance between coords

--- backend/Controller/Entities/Coords.cs
+++ backend/Controller/Entities/Coords.cs
@@ -3,6 +3,8 @@ namespace backend.Controller.Entities
 {
     public struct Coords
     {
+        private const double AVERAGE_RADIUS_OF_EARTH_KM = 6371;
+
         public decimal Longitude { get; set; }
         public decimal Latitude { get; set; }
 
@@ -12,14 +14,35 @@ namespace backend.Controller.Entities
             Latitude = latitude;
         }
 
-        private decimal ManhattanDistance(Coords others)
+        private double HaversineDistanceKM(Coords others)
         {
-            return Math.Abs(Longitude - others.Longitude) + Math.Abs(Latitude - others.Latitude);
+            double latDistance = ToRadians(Latitude - others.Latitude);
+            double lngDistance = ToRadians(Longitude - others.Longitude);
+
+            double a =
+                Math.Sin((double)latDistance / 2.0) * Math.Sin((double)latDistance / 2.0)
+              + Math.Cos(ToRadians(Latitude)) * Math.Cos(ToRadians(others.Latitude))
+              * Math.Sin(lngDistance / 2) * Math.Sin(lngDistance / 2);
+
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            return AVERAGE_RADIUS_OF_EARTH_KM * c;
+        }
+
+        private static double ToRadians(decimal x)
+        {
+            return (double)x * Math.PI / 180;
         }
 
         public bool IsNear(Coords others)
         {
-            return (ManhattanDistance(others) <= (decimal)Distances.NEAR);
+            return IsCloser(others, Distances.NEAR);
+        }
+
+        public bool IsCloser(Coords others, Distances distance)
+        {
+            Console.WriteLine(HaversineDistanceKM(others));
+            return (HaversineDistanceKM(others) <= (double)distance);
         }
     }
 }
