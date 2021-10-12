f1c3bdab54744fb3311e6ceac7a11109d84dee47
Audrius Savickas <audriussav@wix.com>
2021-10-12 15:46:57 +0300

    create react native app

---
+++ frontend/wasted-app/App.tsx
@@ -0,0 +1,8 @@
+import { StatusBar } from "expo-status-bar"
+import { View, Text, Button } from "react-native-ui-lib"
+import React from "react"
+import { MainScreen } from "./src/components/MainScreen.tsx/MainScreen"
+
+export default function App() {
+  return <MainScreen />
+}
