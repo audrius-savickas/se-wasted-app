b79709a63102f9bc43e8d3f9509c3ed595276c2f
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-10-15 13:19:26 +0300

    Add console app to  test a little bit

---
+++ backend/ConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Persistence;
+using Persistence.Repositories;
+using Services.Services;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbConfiguration = DBConfiguration.Instance;
+
+            // Initialize repositories
+            var typeOfFoodRepository = new TypeOfFoodRepository
+            (
+                dbConfiguration.PathToTypesOfFoodFile
+            );
+            var foodRepository = new FoodRepository
+            (
+                dbConfiguration.PathToFoodsFile
+            );
+            var restaurantRepository = new RestaurantRepository
+            (
+                dbConfiguration.PathToRestaurantsFile
+            );
+
+            // Initialize services
+            var typeOfFoodService = new TypeOfFoodService
+            (
+                typeOfFoodRepository
+            );
+            var foodService = new FoodService
+            (
+                foodRepository,
+                restaurantRepository,
+                typeOfFoodRepository
+            );
+            var restaurantService = new RestaurantService
+            (
+                restaurantRepository
+            );
+
+            // Usage
+        }
+    }
+}
