f901101963d322df28d7e23cd5e4450a942e275f
DominykasSip <90202955+DominykasSip@users.noreply.github.com>
2021-10-17 23:01:39 +0300

    Add address to restaurant info (#115)
    * Add address to restaurant entity
    
    * Add address to RestaurantDto

--- backend/Contracts/DTOs/RestaurantDto.cs
+++ backend/Contracts/DTOs/RestaurantDto.cs
@@ -6,14 +6,18 @@ namespace Contracts.DTOs
     {
         public Coords Coords { get; set; }
 
+        public string Address { get; set; }
+
         public RestaurantDto
         (
             string id,
             string name,
+            string address,
             Coords coords
         )
             : base(id, name)
         {
+            Address = address;
             Coords = coords;
         }
 
@@ -23,6 +27,7 @@ namespace Contracts.DTOs
             (
                 restaurant.Id,
                 restaurant.Name,
+                restaurant.Address,
                 restaurant.Coords
             );
         }
