7a9f2a937ef0584345e84b17c68148582c3fabe5
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-10-18 12:19:50 +0300

    Add simple restaurant controller

--- backend/WebApi/Controllers/RestaurantController.cs
+++ backend/WebApi/Controllers/RestaurantController.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using Contracts.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+using System.Collections.Generic;
+using System.Net;
+
 namespace WebApi.Controllers
 {
-    public class RestaurantController
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
     {
-        public RestaurantController()
+        private readonly IRestaurantService _restaurantService;
+
+        public RestaurantController(IRestaurantService restaurantService)
         {
+            _restaurantService = restaurantService;
+        }
+
+        /// <summary>
+        /// Retrieve all restaurants
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet(Name = nameof(GetAllRestaurants))]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(IEnumerable<RestaurantDto>))]
+        public IActionResult GetAllRestaurants()
+        {
+            var restaurants = _restaurantService.GetAllRestaurants();
+
+            return Ok(restaurants);
         }
     }
 }
