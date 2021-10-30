20438c2cf3afd5b8cbb781444ba83a2058ddc475
dominykas s <dosipas@gmail.com>
2021-10-30 17:19:52 +0300

    Added exceptions to CoordHelper

--- backend/Domain/Helpers/CoordsHelper.cs
+++ backend/Domain/Helpers/CoordsHelper.cs
@@ -10,17 +10,24 @@ namespace Domain.Helpers
 
         private static double HaversineDistanceKM(Coords one, Coords others)
         {
-            double latDistance = ToRadians(one.Latitude - others.Latitude);
-            double lngDistance = ToRadians(one.Longitude - others.Longitude);
+            if(one == null || others == null)
+            {
+                throw new Exception("Invalid Coordinates");
+            }
+            else
+            {
+                double latDistance = ToRadians(one.Latitude - others.Latitude);
+                double lngDistance = ToRadians(one.Longitude - others.Longitude);
 
-            double a =
-                Math.Sin((double)latDistance / 2.0) * Math.Sin((double)latDistance / 2.0)
-              + Math.Cos(ToRadians(one.Latitude)) * Math.Cos(ToRadians(others.Latitude))
-              * Math.Sin(lngDistance / 2) * Math.Sin(lngDistance / 2);
+                double a =
+                    Math.Sin((double)latDistance / 2.0) * Math.Sin((double)latDistance / 2.0)
+                  + Math.Cos(ToRadians(one.Latitude)) * Math.Cos(ToRadians(others.Latitude))
+                  * Math.Sin(lngDistance / 2) * Math.Sin(lngDistance / 2);
 
-            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+                double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
 
-            return AVERAGE_RADIUS_OF_EARTH_KM * c;
+                return AVERAGE_RADIUS_OF_EARTH_KM * c;
+            }
         }
 
         private static double ToRadians(decimal x)
