e6944d0764c46fd03c9278d9a437f7dbf156288b
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-11-02 18:34:04 +0200

    Add image entity

---
+++ backend/Domain/Entities/Image.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Domain.Entities
+{
+    public class Image
+    {
+        public Stream ImageStream { get; set; }
+        public string Filename { get; set; }
+
+        public Image(Stream imageStream, string filename)
+        {
+            ImageStream = imageStream;
+            Filename = filename;
+        }
+    }
+}
---
+++ backend/Persistence/Interfaces/IImageService.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace Persistence.Interfaces
+{
+    public interface IImageService
+    {
+    }
+}
