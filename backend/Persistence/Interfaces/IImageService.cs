899d38eb98395d4054c3e7b2f82acb87cd5b3ad2
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-11-02 18:35:41 +0200

    Add service to save read images

--- backend/Persistence/Interfaces/IImageService.cs
+++ backend/Persistence/Interfaces/IImageService.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Domain.Entities;
+
 namespace Persistence.Interfaces
 {
     public interface IImageService
     {
+        void SaveImage(Image image);
+        Image ReadImage(string imageSrc);
     }
 }
---
+++ backend/Persistence/Services/ImageService.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Persistence.Interfaces;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Persistence.Services
+{
+    public class ImageService : IImageService
+    {
+        public ImageService()
+        {
+        }
+
+        public Image ReadImage(string imageSrc)
+        {
+            return new Image
+            (
+                new FileStream(imageSrc, FileMode.Open),
+                imageSrc
+            );
+        }
+
+        public void SaveImage(Image image)
+        {
+            using FileStream destination = File.Create(image.Filename);
+            image.ImageStream.CopyTo(destination);
+        }
+    }
+}
