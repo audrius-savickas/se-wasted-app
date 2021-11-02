c2579e85c15e4fea9530d9fc60b5ca4791069c0d
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-11-02 18:37:25 +0200

    Add neccesary props to manage images

--- backend/Contracts/DTOs/RestaurantDto.cs
+++ backend/Contracts/DTOs/RestaurantDto.cs
@@ -5,31 +5,35 @@ namespace Contracts.DTOs
     public class RestaurantDto : BaseDto
     {
         public Coords Coords { get; set; }
-
         public string Address { get; set; }
+        public Image Image { get; set; }
 
         public RestaurantDto
         (
             string id,
             string name,
             string address,
-            Coords coords
+            Coords coords,
+            Image? image = null
         )
             : base(id, name)
         {
             Address = address;
             Coords = coords;
+            Image = image;
         }
 
-        public static RestaurantDto FromEntity(Restaurant restaurant)
+        public static RestaurantDto FromEntity(Restaurant restaurant, Image? image = null)
         {
             return new RestaurantDto
             (
                 restaurant.Id,
                 restaurant.Name,
                 restaurant.Address,
-                restaurant.Coords
+                restaurant.Coords,
+                image
             );
         }
+
     }
 }
--- backend/Domain/Entities/Restaurant.cs
+++ backend/Domain/Entities/Restaurant.cs
@@ -5,19 +5,27 @@ namespace Domain.Entities
     public class Restaurant : BaseEntity
     {
         public Coords Coords { get; set; }
-
         public string Address { get; set; }
-
         public Credentials Credentials { get; set; }
+        public string ImageSrc { get; set; }
 
         public Restaurant() : base() { }
 
-        public Restaurant(string id, string name, string address, Coords coords, Credentials creds)
+        public Restaurant
+        (
+            string id,
+            string name,
+            string address,
+            Coords coords,
+            Credentials creds,
+            string imageSrc
+        )
             : base(id, name)
         {
             Address = address;
             Coords = coords;
             Credentials = creds;
+            ImageSrc = imageSrc;
         }
 
         public bool IsNear(Coords coords)
