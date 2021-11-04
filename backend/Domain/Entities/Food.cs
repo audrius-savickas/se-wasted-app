c3dab357e0f35821e43f249c9714a3b7edd0e5dd
dominykas s <dosipas@gmail.com>
2021-11-04 17:16:38 +0300

    Added image url to food entity

--- backend/Domain/Entities/Food.cs
+++ backend/Domain/Entities/Food.cs
@@ -8,7 +8,10 @@ namespace Domain.Entities
         public decimal Price { get; set; }
         public DateTime CreatedAt { get; set; }
         public string IdRestaurant { get; set; }
+        public string ImageURL { get; set; }
         public virtual IEnumerable<TypeOfFood> TypesOfFood { get; set; }
+
+        private const string DEFAULT_IMAGE_URL = "todo";
 
         public Food() : base() { }
 
@@ -19,12 +22,14 @@ namespace Domain.Entities
             decimal price,
             string idRestaurant,
             IEnumerable<TypeOfFood> typesOfFood,
-            DateTime? createdAt = null
+            DateTime? createdAt = null,
+            string? imageURL = null
         )
             : base(id, name)
         {
             Price = price;
             CreatedAt = createdAt ?? DateTime.Now;
+            ImageURL = imageURL ?? DEFAULT_IMAGE_URL;
             IdRestaurant = idRestaurant;
             TypesOfFood = typesOfFood;
         }
