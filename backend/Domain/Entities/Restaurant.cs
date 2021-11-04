d27504f9436507432fa5d8a0a4fa9c38dbad55bf
dominykas s <dosipas@gmail.com>
2021-11-05 00:49:10 +0300

    Changed default image URL

--- backend/Domain/Entities/Restaurant.cs
+++ backend/Domain/Entities/Restaurant.cs
@@ -13,7 +13,7 @@ namespace Domain.Entities
         
         public string ImageURL { get; set; }
 
-        private const string DEFAULT_IMAGE_URL = "https://www.google.lt/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ANo-Image-Placeholder.svg&psig=AOvVaw1uR0Zny7PYJn8sTl750JMW&ust=1636147269746000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKCc6rvS__MCFQAAAAAdAAAAABAD";
+        private const string DEFAULT_IMAGE_URL = "https://genesisairway.com/wp-content/uploads/2019/05/no-image.jpg";
 
         public Restaurant() : base() { }
 
