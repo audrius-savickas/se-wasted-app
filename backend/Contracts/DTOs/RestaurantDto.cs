9c92a032eeb53d7c2ad0f363fc785a0ba5d4f69d
dominykas s <dosipas@gmail.com>
2021-11-04 17:42:52 +0300

    Added image url to RestauratDto

--- backend/Contracts/DTOs/RestaurantDto.cs
+++ backend/Contracts/DTOs/RestaurantDto.cs
@@ -9,17 +9,23 @@ namespace Contracts.DTOs
 
         public string Address { get; set; }
 
+        public string ImageURL { get; set; }
+
+        private const string DEFAULT_IMAGE_URL = "todo";
+
         public RestaurantDto
         (
             string id,
             string name,
             string address,
-            Coords coords
+            Coords coords,
+            string? imageURL = null
         )
             : base(id, name)
         {
             Address = address;
             Coords = coords ?? throw new ArgumentNullException(nameof(coords));
+            ImageURL = imageURL ?? DEFAULT_IMAGE_URL;
         }
 
         public static RestaurantDto FromEntity(Restaurant restaurant)
@@ -31,7 +37,8 @@ namespace Contracts.DTOs
                 restaurant.Id,
                 restaurant.Name,
                 restaurant.Address,
-                restaurant.Coords
+                restaurant.Coords,
+                restaurant.ImageURL
             );
         }
     }
