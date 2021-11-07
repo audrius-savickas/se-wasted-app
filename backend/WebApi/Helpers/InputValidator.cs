72046b4a70c6a26e6f92987b470e936732a2724d
DominykasSip <90202955+DominykasSip@users.noreply.github.com>
2021-11-07 14:38:54 +0200

    Created a input validator for api inputs

---
+++ backend/WebApi/Helpers/InputValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.NewFolder
+{
+    public class InputValidator
+    {
+        public bool ValidateSortOrder(string sortOrder, decimal? latitude, decimal? longitude)
+        {
+            if ((latitude == null || longitude == null) && (sortOrder == "dist" || sortOrder == "dist_desc"))
+            {
+                throw new System.ArgumentNullException("Invalid user coordinates.");
+            }
+
+            return sortOrder switch
+            {
+                "dist" => true,
+                "dist_desc" => true,
+                "name" => true,
+                "name_desc" => true,
+                _ => throw new System.ArgumentException("Invalid sort order"),
+            };
+        }
+
+
+
+
+
+    }
+}
