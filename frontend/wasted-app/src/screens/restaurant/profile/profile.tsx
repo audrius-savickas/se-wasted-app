34fbb7b29d9da5829c6cff56ddd88d2c777557e5
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-11-08 10:44:18 +0200

    Add profile basic page

--- frontend/wasted-app/src/screens/restaurant/profile/profile.tsx
+++ frontend/wasted-app/src/screens/restaurant/profile/profile.tsx
@@ -1,11 +1,36 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { Navigation } from "react-native-navigation"
-import {Text, View} from "react-native-ui-lib"
+import {Button, Colors, LoaderScreen, Text, View, TextField} from "react-native-ui-lib"
+import { TextFieldProps } from "react-native-ui-lib/generatedTypes/src/incubator"
+import { getRestaurantById } from "../../../api"
+import { Restaurant } from "../../../api/interfaces"
 import { setHomeRoot } from "../../../services/navigation"
+import { ProfileProps } from "./interfaces"
 
-export const Profile = () => {
+const textFieldCommonValues : TextFieldProps = {
+  editable:false,
+  centered: false
+}
+
+export const Profile = ({
+  restaurantId,
+  restaurantName
+} : ProfileProps ) => {
   
+  const [restaurant, setRestaurant] = useState<Restaurant>({
+    id: restaurantId,
+    name: restaurantName,
+    coords: {
+      latitude: 0.0,
+      longitude: 0.0
+    },
+    address: "",
+  })
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
+    fetchRestaurantInfo()
+
     const listener = Navigation.events().registerNavigationButtonPressedListener(({buttonId}) => {
       if (buttonId === "LOG_OUT") {
         setHomeRoot()
@@ -14,9 +39,52 @@ export const Profile = () => {
     return () => listener.remove()
   }, [])
 
+  const fetchRestaurantInfo = async () => {
+    const response = await getRestaurantById(restaurantId)
+    
+    if (response != null)
+      setRestaurant(response)
+
+    setLoading(false)
+  }
+
   return (
-    <View>
-      <Text>This is profile view</Text>
+    <View flex centerH>
+        {loading 
+          ? 
+          (
+              <LoaderScreen color={Colors.blue30} message="Loading..." />
+          ) 
+          : 
+          (
+            <>
+              <Text text40M>My profile</Text>
+              <TextField 
+                title="Name"
+                textFieldCommonValues
+              />
+              <TextField 
+                title="Location"  
+                textFieldCommonValues 
+              />
+              <TextField 
+                title="Email"  
+                textFieldCommonValues
+              />
+              <Button
+                outline
+                label="Change Password"
+              />
+              <Button
+                outline
+                label="Cancel"
+              />
+              <Button
+                label="Save"
+              />
+            </>
+          )
+        }
     </View>
   )
 }
