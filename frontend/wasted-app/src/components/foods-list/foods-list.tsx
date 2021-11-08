86601d5109915601dace9202204e4026866f3776
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-11-08 18:38:15 +0200

    Show modal on click

--- frontend/wasted-app/src/components/foods-list/foods-list.tsx
+++ frontend/wasted-app/src/components/foods-list/foods-list.tsx
@@ -1,12 +1,24 @@
+import { Zocial } from "@expo/vector-icons"
 import React from "react"
 import {FlatList, ListRenderItemInfo} from "react-native"
+import { TouchableOpacity } from "react-native-gesture-handler"
 import {Food} from "../../api/interfaces"
+import { showFoodInfoModal } from "../../services/navigation"
 import {FoodItem} from "../food-item"
 import {FoodsListProps} from "./interfaces"
 
 export const FoodsList = ({foods}: FoodsListProps) => {
   const renderItem = ({item}: ListRenderItemInfo<Food>) => {
-    return <FoodItem id={item.id} name={item.name} price={item.currentPrice} types={item.typesOfFood} />
+    return (
+      <TouchableOpacity onPress={() => showFoodInfoModal({food: item})}>
+        <FoodItem 
+          id={item.id} 
+          name={item.name} 
+          price={item.currentPrice} 
+          types={item.typesOfFood}
+        />
+      </TouchableOpacity>
+    )
   }
 
   return <FlatList data={foods} renderItem={renderItem} keyExtractor={item => item.id} />
