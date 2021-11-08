ea63084457a10462136889ec2e2bed2bc7b3eacf
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-11-08 12:59:32 +0200

    Update profile page

--- frontend/wasted-app/src/screens/restaurant/profile/profile.tsx
+++ frontend/wasted-app/src/screens/restaurant/profile/profile.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react"
 import { Navigation } from "react-native-navigation"
-import {Button, Colors, LoaderScreen, Text, View, TextField} from "react-native-ui-lib"
+import {Button, Colors, LoaderScreen, Text, View, TextField, Image} from "react-native-ui-lib"
 import { TextFieldProps } from "react-native-ui-lib/generatedTypes/src/incubator"
 import { getRestaurantById } from "../../../api"
 import { Restaurant } from "../../../api/interfaces"
 import { setHomeRoot } from "../../../services/navigation"
 import { ProfileProps } from "./interfaces"
+
+const imageUrl = "https://www.bbc.co.uk/staticarchive/4924b0772cad94008653b216980c869e1d4ec953.jpg"
 
 const textFieldCommonValues : TextFieldProps = {
   editable:false,
@@ -26,6 +28,8 @@ export const Profile = ({
     },
     address: "",
   })
+  const [updatedRestaurant, setUpdatedRestaurant] = useState<Restaurant>(restaurant)
+  
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
@@ -49,40 +53,66 @@ export const Profile = ({
   }
 
   return (
-    <View flex centerH>
+    <View flex margin-s3>
         {loading 
           ? 
           (
+            <View flex centerH>
               <LoaderScreen color={Colors.blue30} message="Loading..." />
+            </View>
           ) 
           : 
           (
-            <>
-              <Text text40M>My profile</Text>
-              <TextField 
-                title="Name"
-                textFieldCommonValues
-              />
-              <TextField 
-                title="Location"  
-                textFieldCommonValues 
-              />
-              <TextField 
-                title="Email"  
-                textFieldCommonValues
-              />
-              <Button
-                outline
-                label="Change Password"
-              />
-              <Button
-                outline
-                label="Cancel"
-              />
-              <Button
-                label="Save"
-              />
-            </>
+            <View flex margin-s3>
+              <View flex center>
+                <Text center text40M marginT-10>My profile</Text>
+                <Image
+                  marginT-s2
+                  source={{
+                    uri: imageUrl, 
+                    height: 200, 
+                    width: 330
+                  }} 
+                />
+              </View>
+              <View flex left marginT-10
+                style={{
+                  flexDirection: "column",
+                  justifyContent: "center"
+                }}
+              >
+                <TextField
+                  title="Name"
+                  placeholder={restaurant.name}
+                  textFieldCommonValues
+                  />
+                <TextField
+                  title="Location"
+                  placeholder={restaurant.address}  
+                  textFieldCommonValues 
+                  />
+                  </View>
+                <Button
+                  outline
+                  label="Change Password"
+                />
+                <View 
+                  flex
+                  style={{
+                    flexDirection: "row",
+                    alignItems: 'center',
+                    justifyContent: 'space-evenly',
+                  }}
+                >
+                  <Button
+                    link={true}
+                    label="Cancel"
+                  />
+                  <Button
+                    label="Save"
+                  />
+                </View>
+            </View>
           )
         }
     </View>
