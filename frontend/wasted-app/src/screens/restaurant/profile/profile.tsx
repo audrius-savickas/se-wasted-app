ce9a75b460b38c795a931d1fb374ccf831819446
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-11-08 13:31:19 +0200

    Add functionality to send the form

--- frontend/wasted-app/src/screens/restaurant/profile/profile.tsx
+++ frontend/wasted-app/src/screens/restaurant/profile/profile.tsx
@@ -2,12 +2,10 @@ import React, { useEffect, useState } from "react"
 import { Navigation } from "react-native-navigation"
 import {Button, Colors, LoaderScreen, Text, View, TextField, Image} from "react-native-ui-lib"
 import { TextFieldProps } from "react-native-ui-lib/generatedTypes/src/incubator"
-import { getRestaurantById } from "../../../api"
+import { getRestaurantById, updateRestaurant as updateRestaurantCall } from "../../../api"
 import { Restaurant } from "../../../api/interfaces"
 import { setHomeRoot } from "../../../services/navigation"
 import { ProfileProps } from "./interfaces"
-
-const imageUrl = "https://www.bbc.co.uk/staticarchive/4924b0772cad94008653b216980c869e1d4ec953.jpg"
 
 const textFieldCommonValues : TextFieldProps = {
   editable:false,
@@ -27,10 +25,11 @@ export const Profile = ({
       longitude: 0.0
     },
     address: "",
+    imageURL: ""
   })
   const [updatedRestaurant, setUpdatedRestaurant] = useState<Restaurant>(restaurant)
-  
   const [loading, setLoading] = useState(true)
+  const [isSaveButtonDisabled, setIsSaveButtonDisabled] = useState(false)
 
   useEffect(() => {
     fetchRestaurantInfo()
@@ -46,10 +45,36 @@ export const Profile = ({
   const fetchRestaurantInfo = async () => {
     const response = await getRestaurantById(restaurantId)
     
-    if (response != null)
+    if (response != null) {
       setRestaurant(response)
+      setUpdatedRestaurant(response)
+    }
 
     setLoading(false)
+  }
+
+  const updateRestaurant = async () => {
+    setIsSaveButtonDisabled(true)
+    await updateRestaurantCall(updatedRestaurant)
+    setIsSaveButtonDisabled(false)
+  }
+
+  const onChangeName = (name : string) => {
+   setUpdatedRestaurant({
+      ...updatedRestaurant,
+      name
+    }) 
+  }
+
+  const onChangeAddress = (address : string) => {
+    setUpdatedRestaurant({
+      ...updatedRestaurant,
+      address
+    })
+  }
+
+  const cancelUpdate = () => {
+    setUpdatedRestaurant(restaurant)
   }
 
   return (
@@ -69,7 +94,7 @@ export const Profile = ({
                 <Image
                   marginT-s2
                   source={{
-                    uri: imageUrl, 
+                    uri: restaurant.imageURL, 
                     height: 200, 
                     width: 330
                   }} 
@@ -83,12 +108,14 @@ export const Profile = ({
               >
                 <TextField
                   title="Name"
-                  placeholder={restaurant.name}
+                  onChangeText={onChangeName}
+                  value={updatedRestaurant.name}
                   textFieldCommonValues
                   />
                 <TextField
                   title="Location"
-                  placeholder={restaurant.address}  
+                  onChangeText={onChangeAddress}
+                  value={updatedRestaurant.address}  
                   textFieldCommonValues 
                   />
                   </View>
@@ -107,9 +134,12 @@ export const Profile = ({
                   <Button
                     link={true}
                     label="Cancel"
+                    onPress={cancelUpdate}
                   />
                   <Button
                     label="Save"
+                    disabled={isSaveButtonDisabled}
+                    onPress={updateRestaurant}
                   />
                 </View>
             </View>
