46b09ef8c60343293b8adb20374ec42dce2b16fd
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-11-11 19:50:44 +0200

    Basic form

--- frontend/wasted-app/src/screens/restaurant/addFood/addFood.tsx
+++ frontend/wasted-app/src/screens/restaurant/addFood/addFood.tsx
@@ -1,8 +1,14 @@
 import React, { useEffect } from "react"
 import { Navigation } from "react-native-navigation"
-import { Text } from "react-native-ui-lib"
+import { Text, View, TextField, TextArea, Button } from "react-native-ui-lib"
+import { TextFieldProps } from "react-native-ui-lib/generatedTypes/src/incubator"
 import { setHomeRoot } from "../../../services/navigation"
 import { AddFoodScreenProps } from "./interfaces"
+
+const textFieldCommonValues : TextFieldProps = {
+  editable:false,
+  centered: false
+}
 
 export const AddFood = ({} : AddFoodScreenProps) => {
   
@@ -16,6 +22,41 @@ export const AddFood = ({} : AddFoodScreenProps) => {
   }, [])
 
   return (
-    <Text>Add food</Text>
+      <View flex margin-s3>
+        <Text center text40M marginT-10>My profile</Text>
+        <View flex left marginT-10 marginL-10
+          style={{
+            flexDirection: "column",
+            justifyContent: "center"
+          }}
+        >
+          <TextField
+            title="Name *"
+            textFieldCommonValues
+          />
+          <Text>Description</Text>
+          <TextArea />
+          <TextField
+            title="Image url"
+            textFieldCommonValues
+          />
+        </View>
+        <View 
+          flex
+          style={{
+            flexDirection: "row",
+            alignItems: 'center',
+            justifyContent: 'space-evenly',
+          }}
+        >
+          <Button
+            link
+            label="Reset"
+          />
+          <Button
+            label="Save"
+          />
+        </View>
+      </View>
   )
 }
