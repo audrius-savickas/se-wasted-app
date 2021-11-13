bdf9cb70b12d1ebe87a69de36c7bf76bcf34c4e7
Audrius Savickas <audriussav@wix.com>
2021-11-13 21:43:02 +0200

    Food info modal improvements (#199)
    * show starting price
    
    * Price indicator bar
    
    * some price indicator design changes
    
    * add description about price decreasal
    
    * add description section
    
    * make description an expandable section
    
    * cleanup
    
    * make border between colors flat in PriceIndicator
    
    * add shadow to image

---
+++ frontend/wasted-app/src/components/price-indicator/price-indicator.tsx
@@ -0,0 +1,55 @@
+import React, {useState} from "react"
+import {LayoutChangeEvent, StyleSheet} from "react-native"
+import {Colors, Spacings, Text, View} from "react-native-ui-lib"
+import {formatPrice} from "../../utils/currency"
+import {PriceIndicatorProps} from "./interfaces"
+
+export const PriceIndicator = ({currentPrice, minimumPrice, maximumPrice}: PriceIndicatorProps) => {
+  const [containerWidth, setContainerWidth] = useState(100)
+
+  const progress = (currentPrice - minimumPrice) / (maximumPrice - minimumPrice)
+
+  const getContainerWidth = (event: LayoutChangeEvent) => {
+    setContainerWidth(event.nativeEvent.layout.width)
+  }
+
+  const getProgressStyle = () => {
+    return StyleSheet.create({
+      style: {
+        left: containerWidth * progress
+      }
+    }).style
+  }
+
+  return (
+    <>
+      <View br30 bg-green20 centerV style={styles.outer} onLayout={getContainerWidth}>
+        <View bg-red20 style={[styles.progress, getProgressStyle()]}></View>
+      </View>
+      <View row marginT-2>
+        <Text flex>Min: {formatPrice(minimumPrice)}</Text>
+        <Text>Max: {formatPrice(maximumPrice)}</Text>
+      </View>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  outer: {
+    overflow: "hidden",
+    borderColor: Colors.grey20,
+    borderWidth: 1,
+    height: Spacings.s4,
+    opacity: 0.8
+  },
+  progress: {
+    position: "absolute",
+    height: "100%",
+    width: "100%",
+    borderColor: Colors.grey20,
+    borderLeftWidth: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden"
+  }
+})
