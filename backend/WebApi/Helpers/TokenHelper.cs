1c94c5168b4bc8fe9b2546fb5fc95b392167fd38
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-11-21 19:47:30 +0200

    Add implementation of token generator

--- backend/WebApi/Helpers/TokenHelper.cs
+++ backend/WebApi/Helpers/TokenHelper.cs
@@ -1,10 +1,54 @@
-﻿using System;
+﻿using Contracts.DTOs;
+using Domain.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebApi.Options;
+
 namespace WebApi.Helpers
 {
-    public class TokenHelper
+    public class TokenHelper : ITokenHelper
     {
-        public TokenHelper()
+        private readonly TokenOptions _tokenOptions;
+
+        public TokenHelper(IOptions<TokenOptions> tokenOptions)
         {
+            _tokenOptions = tokenOptions.Value;
+        }
+
+        public dynamic GenerateToken(Mail mail, RestaurantDto restaurant)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, mail.Value),
+                new Claim(ClaimTypes.NameIdentifier, restaurant.Id),
+                new Claim(JwtRegisteredClaimNames.Nbf, new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds().ToString()),
+                new Claim(JwtRegisteredClaimNames.Exp, new DateTimeOffset(DateTime.Now.AddDays(1)).ToUnixTimeSeconds().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                new JwtHeader(
+                    new SigningCredentials(
+                        new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(_tokenOptions.SecurityKey)
+                        ),
+                        SecurityAlgorithms.HmacSha256
+                    )
+                ),
+                new JwtPayload(claims)
+            );
+
+            var output = new
+            {
+                Access_Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Email = mail.Value
+            };
+
+            return output;
         }
     }
 }
