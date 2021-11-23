3fe1fc76c1bd187ae68caea47895392f84e46815
Donatas Pučinskas <pucinskas.donatas@gmail.com>
2021-11-23 11:42:46 +0200

    Basic implementation of entities

---
+++ backend/Domain/Entities/FoodEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class FoodEntity : Entity
+    {
+        public FoodEntity()
+        {
+            TypesOfFood = new HashSet<TypeOfFoodEntity>();
+        }
+        public string Name { get; set; }
+        public string RestaurantId { get; set; }
+        public decimal StartingPrice { get; set; }
+        public decimal MinimumPrice { get; set; }
+        public DateTime StartDecreasingAt { get; set; }
+        public double IntervalTimeInMinutes { get; set; }
+        public string DecreaseType { get; set; }
+        public decimal? AmountPerInterval { get; set; }
+        public double? PercentPerInterval { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+
+        public virtual RestaurantEntity Restaurant { get; set; }
+        public virtual ICollection<TypeOfFoodEntity> TypesOfFood { get; set; }
+    }
+}
