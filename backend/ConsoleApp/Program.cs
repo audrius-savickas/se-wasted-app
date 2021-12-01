9e4a3bae145b1dc1796490cb66e8fddc8fbda6f5
Donatas Pučinskas <pucinskas.donatas@gmail.com>
2021-12-01 23:10:58 +0200

    Remove all code from console app

--- backend/ConsoleApp/Program.cs
+++ backend/ConsoleApp/Program.cs
@@ -1,59 +1,10 @@
-﻿using Domain.Entities;
-using Persistence;
-using Persistence.Repositories;
-using Services.Mappers;
-using Services.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace ConsoleApp
+﻿namespace ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var dbConfiguration = DBConfiguration.Instance;
-
-            // Initialize repositories
-            var typeOfFoodRepository = new TypeOfFoodRepository
-            (
-                dbConfiguration.PathToTypesOfFoodFile
-            );
-            var foodRepository = new FoodRepository
-            (
-                dbConfiguration.PathToFoodsFile
-            );
-            var restaurantRepository = new RestaurantRepository
-            (
-                dbConfiguration.PathToRestaurantsFile
-            );
-
-            // Initialize services
-            var typeOfFoodService = new TypeOfFoodService
-            (
-                typeOfFoodRepository
-            );
-            var foodService = new FoodService
-            (
-                foodRepository,
-                restaurantRepository,
-                typeOfFoodRepository
-            );
-            var restaurantService = new RestaurantService
-            (
-                restaurantRepository,
-                foodRepository
-            );
-
-            // Usage
-            /*var food = foodService.GetAllFood();
-
-            var entity = food.Select(x => x.ToEntity()).ToList();
-
-            var restaurant = restaurantService.GetAllRestaurants();
-
-            Console.WriteLine("success");*/
+            
         }
     }
 }
