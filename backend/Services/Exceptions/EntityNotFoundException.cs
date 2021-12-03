aa80b20eec6195f378fb7ababe1191d9042dff62
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-12-03 12:50:29 +0200

    Make other exceptions extends base business exception

--- backend/Services/Exceptions/EntityNotFoundException.cs
+++ backend/Services/Exceptions/EntityNotFoundException.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Services.Exceptions
 {
-    public class EntityNotFoundException : Exception
+    public class EntityNotFoundException : BusinessException
     {
         public EntityNotFoundException()
         {
