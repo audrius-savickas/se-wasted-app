92b7a0558973fda3923f2f6687e48676ebf0baf3
Donatas Pučinskas <pucinskas.donatas@gmail.com>
2021-12-08 01:07:56 +0200

    Revert testing

--- backend/WebApi/Startup.cs
+++ backend/WebApi/Startup.cs
@@ -14,13 +14,10 @@ using Services.Services;
 using System;
 using System.IO;
 using System.Reflection;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using WebApi.Helpers;
 using Services.Repositories;
 using WebApi.Middleware;
 using Serilog;
-using Microsoft.AspNetCore.Http;
 
 namespace WebApi
 {
@@ -138,11 +135,6 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.Run(async context =>
-            {
-                context.Response.ContentType = "text/plain";
-                await context.Response.WriteAsync($@"SecretName (Name in Key Vault: 'SecretName'){Environment.NewLine}Obtained from Configuration with Configuration[""SecretName""]{Environment.NewLine}Value: {Configuration["SecretName"]}{Environment.NewLine}{Environment.NewLine}Section:SecretName (Name in Key Vault: 'EmailOptions--Host'){Environment.NewLine}Obtained from Configuration with Configuration[""EmailOptions:Host""]{Environment.NewLine}Value: {Configuration["EmailOptions:Host"]}{Environment.NewLine}{Environment.NewLine}Section:SecretName (Name in Key Vault: 'Section--SecretName'){Environment.NewLine}Obtained from Configuration with Configuration.GetSection(""Section"")[""SecretName""]{Environment.NewLine}Value: {Configuration.GetSection("Section")["SecretName"]}");
-            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -171,8 +163,6 @@ namespace WebApi
             {
                 endpoints.MapControllers();
             });
-
-            
         }
     }
 }
