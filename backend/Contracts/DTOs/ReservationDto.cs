5d96f17ecb4702b23b3898f5802e09a38663c8e8
Donatas Pučinskas <pucinskas.donatas@gmail.com>
2021-12-13 16:12:48 +0200

    Add reservation dto

---
+++ backend/Contracts/DTOs/ReservationDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.DTOs
+{
+    public class ReservationDto
+    {
+        public string Id { get; set; }
+        public DateTime ReservedAt { get; set; }
+        public decimal Price { get; set; }
+        public string FoodId { get; set; }
+        public string RestaurantId { get; set; }
+        public string CustomerId { get; set; }
+        public ReservationDto(
+            string id,
+            string foodId,
+            string restaurantId,
+            string customerId,
+            DateTime reservedAt,
+            decimal price)
+        {
+            Id = id;
+            ReservedAt = reservedAt;
+            FoodId = foodId;
+            RestaurantId = restaurantId;
+            Price = price;
+            CustomerId = customerId;
+        }
+    }
+}
---
+++ backend/Persistence/Mappers/ReservationMapper.cs
@@ -0,0 +1,26 @@
+﻿using Contracts.DTOs;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Mappers
+{
+    public static class ReservationMapper
+    {
+        public static ReservationDto ToDTO(this Reservation from)
+        {
+            _ = from ?? throw new ArgumentNullException(nameof(from));
+
+            return new ReservationDto(
+                from.Id,
+                from.Food.Id,
+                from.Restaurant.Id,
+                from.Customer.Id,
+                from.ReservedAt,
+                from.Price);
+        }
+    }
+}
