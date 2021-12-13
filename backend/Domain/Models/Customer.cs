423de6c1f01a7ba1ef4adadae0d8f6aea81eb89e
Donatas Pučinskas <pucinskas.donatas@gmail.com>
2021-12-13 16:03:41 +0200

    Add cutomer and reservation models

---
+++ backend/Domain/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Customer : BaseModel
+    {
+        public IEnumerable<Reservation> Reservations { get; set; }
+        public Credentials Credentials { get; set; }
+
+        public Customer(
+            string id,
+            string name,
+            IEnumerable<Reservation> reservations,
+            Credentials credentials) : base(id, name)
+        {
+            Reservations = reservations;
+            Credentials = credentials ?? throw new ArgumentNullException(nameof(credentials));
+        }
+    }
+}
---
+++ backend/Domain/Models/Reservation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Reservation
+    {
+        public string Id { get; set; }
+        public DateTime ReservedAt { get; set; }
+        public decimal Price { get; set; }
+        public Food Food { get; set; }
+        public Restaurant Restaurant { get; set; }
+        public Customer Customer { get; set; }
+        public Reservation(
+            string id,
+            Food food,
+            Restaurant restaurant, 
+            Customer customer)
+        {
+            Id = id;
+            ReservedAt = DateTime.Now;
+            Food = food;
+            Restaurant = restaurant;
+            Price = food.CalculateCurrentPrice();
+            Customer = customer;
+        }
+    }
+}
