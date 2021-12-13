2fb46411e9a6f3bc6089a9fd2b853dffe5882782
Donatas Pučinskas <pucinskas.donatas@gmail.com>
2021-12-13 18:01:12 +0200

    Add customer entity

---
+++ backend/Persistence/Mappers/CustomerMapper.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entities;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Mappers
+{
+    public static class CustomerMapper
+    {
+        public static Customer ToDomain(this CustomerEntity from)
+        {
+            _ = from ?? throw new ArgumentNullException(nameof(from));
+
+            Credentials credentials = new Credentials
+            {
+                Mail = new Mail { Value = from.Mail },
+                Password = new Password { Value = from.Password }
+            };
+
+            return new Customer(
+                from.Id.ToString(),
+                from.FirstName,
+                from.LastName,
+                null,
+                credentials);
+        }
+
+        public static CustomerEntity ToEntity(this Customer from)
+        {
+            _ = from ?? throw new ArgumentNullException(nameof(from));
+
+            return new CustomerEntity
+            {
+                Id = Guid.Parse(from.Id),
+                FirstName = from.FirstName,
+                LastName = from.LastName,
+                Mail = from.Credentials.Mail.Value,
+                Password = from.Credentials.Password.Value,
+            };
+        }
+    }
+}
