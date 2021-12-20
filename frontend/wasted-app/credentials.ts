ff692204c3e13da5f61730dd778632d63b2ca2e9
audriussav <umarasss@gmail.com>
2021-12-20 18:42:03 +0200

    remove ios client id from credentials

--- frontend/wasted-app/credentials.ts
+++ frontend/wasted-app/credentials.ts
@@ -1,2 +1,2 @@
 export const GOOGLE_MAPS_API_KEY = ""
-export const GOOGLE_IOS_CLIENT_ID = "834850407777-uv37m0m83itkc63p628t4hs52vabfrnh.apps.googleusercontent.com"
+export const GOOGLE_IOS_CLIENT_ID = ""
